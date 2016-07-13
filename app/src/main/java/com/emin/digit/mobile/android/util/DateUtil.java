01f4995c2b99083c8519c02f30446d2a8c86679f
sokasyn <sokasyn@qq.com>
2016-07-13 18:27:09 +0800

    Util

---
+++ app/src/main/java/com/emin/digit/mobile/android/util/DateUtil.java
@@ -0,0 +1,9 @@
+package com.emin.digit.mobile.android.util;
+
+/**
+ * Created by Samson on 16/7/13.
+ *
+ * 日期时间的处理工具
+ */
+public class DateUtil {
+}
---
+++ app/src/main/java/com/emin/digit/mobile/android/util/JSONUtil.java
@@ -0,0 +1,7 @@
+package com.emin.digit.mobile.android.util;
+
+/**
+ * Created by Samson on 16/7/13.
+ */
+public class JSONUtil {
+}
