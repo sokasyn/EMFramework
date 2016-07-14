3a2e46060dd230a2b66e2eb67e9052f440a9430d
sokasyn <sokasyn@qq.com>
2016-07-14 18:48:43 +0800

    EMFramework storage

---
+++ app/src/main/java/com/emin/digit/mobile/android/storage/preference/PreferenceHelper.java
@@ -0,0 +1,32 @@
+package com.emin.digit.mobile.android.storage.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+/**
+ * Created by Samson on 16/7/14.
+ */
+public class PreferenceHelper {
+
+    // 优化:Context
+    public static void WriteSharedPreferences(Context context, String prefsName, String name, Object value) {
+        if (name == null || value == null) {
+            return;
+        }
+        SharedPreferences user = context.getSharedPreferences(prefsName, 0);
+        Editor editor = user.edit();
+        if (value instanceof Integer) {
+            editor.putInt(name, Integer.parseInt(value.toString()));
+        } else if (value instanceof Long) {
+            editor.putLong(name, Long.parseLong(value.toString()));
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(name, Boolean.parseBoolean(value.toString()));
+        } else if (value instanceof String) {
+            editor.putString(name, value.toString());
+        } else if (value instanceof Float) {
+            editor.putFloat(name, Float.parseFloat(value.toString()));
+        }
+        editor.commit();
+    }
+}
